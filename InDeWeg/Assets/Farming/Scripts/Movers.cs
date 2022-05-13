75e1b91ae467e0bcdc9519c6a1a6defac86db44b
Douche-Box <Maykelvboven05@gmail.com>
2022-05-13 10:49:37 +0200

    More scripts

---
+++ InDeWeg/Assets/Farming/Scripts/Movers.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movers : MonoBehaviour
+{
+    public Vector3 move;
+    public float horizontal;
+    public float vertical;
+    public float speed;
+
+    Rigidbody rb;
+
+    public RaycastHit hit;
+    public GameObject player;
+
+    public GameObject fpsCam;
+
+    void Start()
+    {
+        speed = 1;
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        move.x = horizontal;
+        move.z = vertical;
+
+        transform.Translate(move * speed * Time.deltaTime * 10);
+    }
+}
