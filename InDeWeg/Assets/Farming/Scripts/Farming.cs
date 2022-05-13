ddc83d7d9eeb8c46a1031dd6a335adf2b1638081
Douche-Box <Maykelvboven05@gmail.com>
2022-05-13 11:22:24 +0200

    more scripts

--- InDeWeg/Assets/Farming/Scripts/Farming.cs
+++ InDeWeg/Assets/Farming/Scripts/Farming.cs
@@ -6,12 +6,24 @@ public class Farming : MonoBehaviour
 {
     public RaycastHit hit;
 
+    public GameObject cam3;
+
+    public Vector3 direction;
+
+    public LayerMask mask;
     void Start()
     {
         
     }
     void Update()
     {
+        // maybe GetButton
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Physics.Raycast(cam3.transform.position, Vector3.forward, out hit, 10.0f) && hit.transform.gameObject.tag == "FarmingTile")
+            {
 
+            }
+        }
     }
 }
