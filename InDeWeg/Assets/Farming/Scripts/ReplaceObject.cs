50f33b8fc8d8078d3c1298e8993106d51f519109
Douche-Box <Maykelvboven05@gmail.com>
2022-05-19 11:08:28 +0200

    Deletion Code 2

--- InDeWeg/Assets/Farming/Scripts/ReplaceObject.cs
+++ InDeWeg/Assets/Farming/Scripts/ReplaceObject.cs
@@ -13,6 +13,9 @@ public class ReplaceObject : MonoBehaviour
     public GameObject growth1;
     public GameObject growth2;
 
+    public GameObject gTiledClone;
+    public GameObject pPlantedClone;
+
     public Transform origin;
 
     public int groundstates;
@@ -21,6 +24,8 @@ public class ReplaceObject : MonoBehaviour
     void Start()
     {
         groundstates = 0;
+
+        gTiledClone = Instantiate(groundTile, transform.position, Quaternion.identity);
     }
     // Update is called once per frame
     void Update()
@@ -34,50 +39,58 @@ public class ReplaceObject : MonoBehaviour
         {
             groundstates = 1;
 
-            GameObject gTClone = Instantiate(groundTileTilled, transform.position, Quaternion.identity);
+            Destroy(gTiledClone);
+
+            gTiledClone = Instantiate(groundTileTilled, transform.position, Quaternion.identity);
         }
         // waterd ground
         if (Toolstate == "WateringCan" && groundstates == 0)
         {
             groundstates = 2;
 
-            Instantiate(groundWatered, transform.position, Quaternion.identity);
+            Destroy(gTiledClone);
+
+            gTiledClone = Instantiate(groundWatered, transform.position, Quaternion.identity);
         }
         // waterd tilled ground Water can last
         if (Toolstate == "WateringCan" && groundstates == 1)
         {
             groundstates = 3;
 
-            Instantiate(groundTileWatered, transform.position, Quaternion.identity);
+            Destroy(gTiledClone);
+
+            gTiledClone = Instantiate(groundTileWatered, transform.position, Quaternion.identity);
         }
         // waterd tilled ground Hoe last
         if (Toolstate == "Hoe" && groundstates == 2)
         {
             groundstates = 3;
 
-            Instantiate(groundTileWatered, transform.position, Quaternion.identity);
+            Destroy(gTiledClone);
+
+            gTiledClone = Instantiate(groundTileWatered, transform.position, Quaternion.identity);
         }
         // planting seeds
         if (Toolstate == "Shovel" && groundstates == 3)
         {
             groundstates = 4;
 
-            GameObject seedsClone = Instantiate(seeds, transform.position, Quaternion.identity);
+            pPlantedClone = Instantiate(seeds, transform.position, Quaternion.identity);
             StartCoroutine(Growing1());
-            Destroy(seedsClone, 8f);
+            Destroy(pPlantedClone, 8f);
         }
     }
     public IEnumerator Growing1()
     {
         yield return new WaitForSeconds(8f);
-        GameObject growth1clone = Instantiate(growth1, transform.position, Quaternion.identity);
-        Destroy(growth1clone, 5f);
+        pPlantedClone = Instantiate(growth1, transform.position, Quaternion.identity);
+        Destroy(pPlantedClone, 5f);
         StartCoroutine(Growing2());
     }
     public IEnumerator Growing2()
     {
         yield return new WaitForSeconds(5f);
-        GameObject growth2clone = Instantiate(growth2, transform.position, Quaternion.identity);
+        pPlantedClone = Instantiate(growth2, transform.position, Quaternion.identity);
     }
 }
 
