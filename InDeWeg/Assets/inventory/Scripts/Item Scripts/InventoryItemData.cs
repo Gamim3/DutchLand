15e8b18b731518b95ff4ecd442856b58e4bf3a17
BarnyIsPurple <99257812+BarnyIsPurple@users.noreply.github.com>
2022-05-20 14:59:46 +0200

    Inventory BackEnd version 1

---
+++ InDeWeg/Assets/inventory/Scripts/Item Scripts/InventoryItemData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory System/Inventory Item")]
+public class InventoryItemData : ScriptableObject
+{
+    public int iD;
+    public string displayName;
+    [TextArea(4, 4)]
+    public string description;
+    public Sprite icon;
+    public int maxStackSize;
+}
