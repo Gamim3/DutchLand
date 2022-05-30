e6aae706357dc07881cd091579565ee627d27d6d
BarnyIsPurple <99257812+BarnyIsPurple@users.noreply.github.com>
2022-05-30 13:00:22 +0200

    Update inventory 2.1

--- InDeWeg/Assets/inventory/InventorySystem/Scripts/Item Scripts/ItemObjects.cs
+++ InDeWeg/Assets/inventory/InventorySystem/Scripts/Item Scripts/ItemObjects.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public enum ItemType
 {
@@ -13,5 +14,15 @@ public abstract class ItemObjects : ScriptableObject
     public ItemType type;
     [TextArea(15,20)]
     public string description;
+
+    public TMP_Text titleTXT;
+    public TMP_Text descriptionTXT;
+    public TMP_Text costTXT;
+
+    public GameObject stage0;
+    public GameObject stage1;
+    public GameObject stage2;
+    public GameObject stage3;
+    public GameObject stage4;
 
 }
