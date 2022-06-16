7c8beb754faffd9dc3c2e81a666f061519e39537
Douche-Box <Maykelvboven05@gmail.com>
2022-06-16 13:41:22 +0200

    Shop Code impelmentation

--- InDeWeg/Assets/Farming/Scripts/DeliveryManager.cs
+++ InDeWeg/Assets/Farming/Scripts/DeliveryManager.cs
@@ -5,82 +5,40 @@ using UnityEngine;
 public class DeliveryManager : MonoBehaviour
 {
     public GameObject[] deliveryPlace;
-    public int arrayNumberPlace;
-    public Transform deliveryPlaceTrans;
-    public GameObject outline;
+
+    public int numberPlace;
+
+    public GameObject outlineC;
+    public GameObject outlineS;
+    public GameObject outlineR;
+
+    public bool sceneSwitch;
+
     // Start is called before the first frame update
     void Start()
     {
-        arrayNumberPlace = 10;
+        numberPlace = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (sceneSwitch == true)
         {
-            arrayNumberPlace = Random.Range(0, 9);
+            numberPlace = Random.Range(0, 2);
+
+            Instantiate(outlineC, deliveryPlace[numberPlace].transform.position, Quaternion.identity);
+
+            numberPlace = Random.Range(3, 6);
+
+            Instantiate(outlineS, deliveryPlace[numberPlace].transform.position, Quaternion.identity);
+
+            numberPlace = Random.Range(7, 9);
+
+            Instantiate(outlineR, deliveryPlace[numberPlace].transform.position, Quaternion.identity);
+
+            sceneSwitch = false;
         }
-        
-        if (arrayNumberPlace == 0)
-        {
-            deliveryPlaceTrans = deliveryPlace[0].transform;
-            Instantiate(outline, deliveryPlaceTrans.position, Quaternion.identity);
-            arrayNumberPlace = 10;
-        }
-        if (arrayNumberPlace == 1)
-        {
-            deliveryPlaceTrans = deliveryPlace[1].transform;
-            Instantiate(outline, deliveryPlaceTrans.position, Quaternion.identity);
-            arrayNumberPlace = 10;
-        }
-        if (arrayNumberPlace == 2)
-        {
-            deliveryPlaceTrans = deliveryPlace[2].transform;
-            Instantiate(outline, deliveryPlaceTrans.position, Quaternion.identity);
-            arrayNumberPlace = 10;
-        }
-        if (arrayNumberPlace == 3)
-        {
-            deliveryPlaceTrans = deliveryPlace[3].transform;
-            Instantiate(outline, deliveryPlaceTrans.position, Quaternion.identity);
-            arrayNumberPlace = 10;
-        }
-        if (arrayNumberPlace == 4)
-        {
-            deliveryPlaceTrans = deliveryPlace[4].transform;
-            Instantiate(outline, deliveryPlaceTrans.position, Quaternion.identity);
-            arrayNumberPlace = 10;
-        }
-        if (arrayNumberPlace == 5)
-        {
-            deliveryPlaceTrans = deliveryPlace[5].transform;
-            Instantiate(outline, deliveryPlaceTrans.position, Quaternion.identity);
-            arrayNumberPlace = 10;
-        }
-        if (arrayNumberPlace == 6)
-        {
-            deliveryPlaceTrans = deliveryPlace[6].transform;
-            Instantiate(outline, deliveryPlaceTrans.position, Quaternion.identity);
-            arrayNumberPlace = 10;
-        }
-        if (arrayNumberPlace == 7)
-        {
-            deliveryPlaceTrans = deliveryPlace[7].transform;
-            Instantiate(outline, deliveryPlaceTrans.position, Quaternion.identity);
-            arrayNumberPlace = 10;
-        }
-        if (arrayNumberPlace == 8)
-        {
-            deliveryPlaceTrans = deliveryPlace[8].transform;
-            Instantiate(outline, deliveryPlaceTrans.position, Quaternion.identity);
-            arrayNumberPlace = 10;
-        }
-        if (arrayNumberPlace == 9)
-        {
-            deliveryPlaceTrans = deliveryPlace[9].transform;
-            Instantiate(outline, deliveryPlaceTrans.position, Quaternion.identity);
-            arrayNumberPlace = 10;
-        }
+
     }
 }
