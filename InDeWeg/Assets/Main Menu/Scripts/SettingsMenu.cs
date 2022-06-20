a5707bfeebc840c1303ad9d71a77fe313eeaae83
meeskees99 <97054368@st.deltion.nl>
2022-06-21 01:06:26 +0200

    main menu v0.2

--- InDeWeg/Assets/Main Menu/Scripts/SettingsMenu.cs
+++ InDeWeg/Assets/Main Menu/Scripts/SettingsMenu.cs
@@ -1,13 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 public class SettingsMenu : MonoBehaviour
 {
 
-    public void SetVolume (float volume)
+    public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
     {
-        Debug.Log(volume);
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> Options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string Option = resolutions[i].width + "x" + resolutions[i].height;
+            Options.Add(Option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(Options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Debug.Log ("Fullscreen");
+        Screen.fullScreen = isFullscreen;
+    }
 }
