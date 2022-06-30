f6deaa26bc54ba04460e12c840185d9b135682d8
Douche-Box <Maykelvboven05@gmail.com>
2022-06-30 10:20:42 +0200

    Dev Tools try

---
+++ InDeWeg/Assets/Farming/Scripts/DevTools.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevTools : MonoBehaviour
+{
+    public GameObject shopManagerGO;
+    public GameObject InventoryManagerGO;
+    public GameObject requestManagerGO;
+    public GameObject GroundGO;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetButtonDown("Enable Debug 1") && Input.GetButtonDown("Enable Debug 2"))
+        //{
+            //print("o");
+        //}
+    }
+}
